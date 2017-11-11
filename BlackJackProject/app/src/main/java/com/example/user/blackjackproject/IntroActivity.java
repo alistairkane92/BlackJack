e95f78ac3915dfeeb6734b83435596be21b26c92
Alistair Kane <vplelmo@hotmail.co.uk>
2017-11-11 10:43:08 +0000

    adds basic intro activity

---
+++ BlackJackProject/app/src/main/java/com/example/user/blackjackproject/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.user.blackjackproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        button = (Button) findViewById(R.id.button);
+    }
+
+
+    public void goToMain(View view) {
+        Intent i = new Intent(this, GameActivity.class);
+        startActivity(i);
+    }
+}
