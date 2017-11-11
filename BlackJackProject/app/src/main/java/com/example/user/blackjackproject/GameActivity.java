30e462876f224692338ca3db7f966ab01a9119ec
Alistair Kane <vplelmo@hotmail.co.uk>
2017-11-11 11:16:02 +0000

    creates display for gameActivity

--- BlackJackProject/app/src/main/java/com/example/user/blackjackproject/GameActivity.java
+++ BlackJackProject/app/src/main/java/com/example/user/blackjackproject/GameActivity.java
@@ -1,6 +1,5 @@
 package com.example.user.blackjackproject;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +9,7 @@ public class GameActivity extends AppCompatActivity {
     Button startButton;
     Game newGame;
     Deck deck;
+    Dealer dealer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +17,13 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_activity);
 
         newGame = new Game(deck);
-        startButton = (Button) findViewById(R.id.play);
+        dealer = new Dealer("Villain");
+        startButton = (Button) findViewById(R.id.playBtn);
     }
 
     public void startGame(View view) {
         newGame.play();
 
-        
+
     }
 }
