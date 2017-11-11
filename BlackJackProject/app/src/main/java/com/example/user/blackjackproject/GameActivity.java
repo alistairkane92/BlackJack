0e590368d530c8f59167ac59b52053e4d57ba7f3
Alistair Kane <vplelmo@hotmail.co.uk>
2017-11-11 10:55:54 +0000

    changes colour scheme and adds start button

--- BlackJackProject/app/src/main/java/com/example/user/blackjackproject/GameActivity.java
+++ BlackJackProject/app/src/main/java/com/example/user/blackjackproject/GameActivity.java
@@ -1,13 +1,28 @@
 package com.example.user.blackjackproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
+    Button startButton;
+    Game newGame;
+    Deck deck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_activity);
+
+        newGame = new Game(deck);
+        startButton = (Button) findViewById(R.id.play);
+    }
+
+    public void startGame(View view) {
+        newGame.play();
+
+        
     }
 }
