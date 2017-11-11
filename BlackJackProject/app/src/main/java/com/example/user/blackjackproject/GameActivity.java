7308b7a8293ca1d741b128d36e02beb8bcbb8cc1
Alistair Kane <vplelmo@hotmail.co.uk>
2017-11-11 15:11:28 +0000

    codes betBar and separates initialize variables

--- BlackJackProject/app/src/main/java/com/example/user/blackjackproject/GameActivity.java
+++ BlackJackProject/app/src/main/java/com/example/user/blackjackproject/GameActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
     Button startButton;
     Game newGame;
     Deck deck;
     Integer betPlaced;
+
     TextView handDisplayTv;
     TextView handValueTv;
     TextView dealerHandTv;
@@ -18,17 +21,13 @@ public class GameActivity extends AppCompatActivity {
     TextView showWinnerTv;
     TextView checkBustTv;
     TextView showFundsTv;
+    TextView selectedBetTv;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_activity);
-        betPlaced = 0;
+    SeekBar betBar;
 
-
-        deck = new Deck();
-        newGame = new Game(deck);
+    private void initializeVariables(){
         startButton = (Button) findViewById(R.id.playBtn);
+        betBar = (SeekBar) findViewById(R.id.pickABetSb);
         handDisplayTv = (TextView) findViewById(R.id.handDisplayTv);
         handValueTv = (TextView) findViewById(R.id.handValueTv);
         dealerHandTv = (TextView) findViewById(R.id.dealerHandDisplayTv);
@@ -37,7 +36,42 @@ public class GameActivity extends AppCompatActivity {
         checkBustTv = (TextView) findViewById(R.id.checkBustTv);
         showFundsTv = (TextView) findViewById(R.id.displayFunds);
         showFundsTv.setText(newGame.showUserFunds().toString());
+        selectedBetTv = (TextView) findViewById(R.id.selectedBetTV);
+        betPlaced = 0;
+        deck = new Deck();
+        newGame = new Game(deck);
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_activity);
+        initializeVariables();
+
+        selectedBetTv.setText("Bet amount: " + betBar.getProgress() + "/" + betBar.getMax());
+
+
+        betBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+                Toast.makeText(getApplicationContext(), "Why don't you bet a little bit more?", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "PLACE YOUR BET!!!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                selectedBetTv.setText("Bet amount: " + progress + "/" + betBar.getMax());
+                Toast.makeText(getApplicationContext(), "Are you sure that's enough?", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     //steps of Game
